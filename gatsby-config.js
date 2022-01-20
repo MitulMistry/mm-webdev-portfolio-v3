c22745fe5fdd07f1dfc05d9b79746126acb3eed1
Mitul Mistry <MitulMistryDev@gmail.com>
2022-01-19 18:39:41 -0600

    Update metadata for application

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    title: `Mitul Mistry - Web Developer`,
+    description: `Mitul Mistry is a full stack web developer with experience in Rails and React.`,
+    author: `@Jackal_MM`,
+    siteUrl: `https://mitulmistry.com/`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -20,10 +20,10 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `mm-webdev-portfolio-v3`,
+        short_name: `MitulMistryPortfolio`,
         start_url: `/`,
-        background_color: `#663399`,
+        background_color: `#FFFFFF`,
         // This will impact how browsers show your PWA/website
         // https://css-tricks.com/meta-theme-color-and-trickery/
         // theme_color: `#663399`,
