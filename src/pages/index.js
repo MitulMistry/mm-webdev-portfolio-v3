0d8fb453a168b849455b4588d4eff8ba8b3569d4
Mitul Mistry <MitulMistryDev@gmail.com>
2022-01-19 18:51:54 -0600

    Remove unneeded template files

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,12 +19,6 @@ const IndexPage = () => (
       alt="A Gatsby astronaut"
       style={{ marginBottom: `1.45rem` }}
     />
-    <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link> <br />
-      <Link to="/using-ssr">Go to "Using SSR"</Link> <br />
-      <Link to="/using-dsg">Go to "Using DSG"</Link>
-    </p>
   </Layout>
 )
 
