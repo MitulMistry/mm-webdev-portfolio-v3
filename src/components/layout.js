a909fd05c742f225517ec98dac4e38996ec42bd8
Mitul Mistry <MitulMistryDev@gmail.com>
2022-01-19 19:07:08 -0600

    Add Bootstrap styling

--- src/components/layout.js
+++ src/components/layout.js
@@ -10,7 +10,8 @@ import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
-import "./layout.css"
+// import "./layout.css"
+import "bootstrap/dist/css/bootstrap.css"
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import * as React from "react"
-import { Link } from "gatsby"
+
+// import { Link } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
 
 import Layout from "../components/layout"
